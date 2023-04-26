315b3b6755c6c0413408b4e9d01e403261d3334c
Esteban <estebannovilloq@gmail.com>
2023-04-26 10:21:32 -0500

    Initial design

--- paginaCTIC/src/app/app.component.ts
+++ paginaCTIC/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'paginaCTIC';
+
+  imageObject: Array<object> = [{
+    image: 'assets/images/empresa.jpg',
+    alt: 'empresa',
+    title: 'Centrosur'
+  }, {
+    image: 'assets/images/medidores.png', // Support base64 image
+    title: 'Medidores inteligentes', //Optional: You can use this key if want to show image with title
+    alt: 'MedidoresInteligentes', //Optional: You can use this key if want to show image with alt
+    order: 1 //Optional: if you pass this key then slider images will be arrange according @input: slideOrderType
+  }
+  ];
+
 }
