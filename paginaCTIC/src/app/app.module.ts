14e0b8b3ce16a30a182f86b10b4c4e78b267d6db
ronald175 <ronaldandrade456@gmail.com>
2023-04-27 16:00:59 -0500

    slider

--- paginaCTIC/src/app/app.module.ts
+++ paginaCTIC/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgImageSliderModule } from 'ng-image-slider';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @NgModule({
   declarations: [
