25c7e7c0f9fc0ff9e37728ecaa16218560946fd0
ronald175 <ronaldandrade456@gmail.com>
2023-05-02 15:04:43 -0500

    scrollbar

--- paginaCTIC/src/app/app.module.ts
+++ paginaCTIC/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgImageSliderModule } from 'ng-image-slider';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 @NgModule({
   declarations: [
