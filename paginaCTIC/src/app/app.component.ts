8fa86e53aa901c41eb7c3c30933e9c751c978599
Esteban <estebannovilloq@gmail.com>
2023-05-03 14:31:01 -0500

    Added firebase services

--- paginaCTIC/src/app/app.component.ts
+++ paginaCTIC/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'paginaCTIC';
+
+  public mision: string ='';
+
+
+  constructor(private fire : FirebaseService) { 
+    fire.getMision().subscribe(mision => {
+      if (mision.length > 0) {
+        this.mision = mision[0].descripcion;
+        console.log('Mision: ', this.mision);
+      } else {
+        this.mision = "Actualice la mision en la BD de Firebase";
+      }
+    });
+  }
 
   imageObject: Array<object> = [{
     image: 'assets/images/empresa.jpg',
--- paginaCTIC/src/app/services/firebase.service.ts
+++ paginaCTIC/src/app/services/firebase.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  private misionCollection: AngularFirestoreCollection<any>;
+  mision$: Observable<any[]>;
+
+  private visionCollection: AngularFirestoreCollection<any>;
+  vision$: Observable<any[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.misionCollection = afs.collection<any>('mision');
+    this.mision$ = this.misionCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => a.payload.doc.data()))
+    );
+
+    this.visionCollection = afs.collection<any>('vision');
+    this.vision$ = this.visionCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => a.payload.doc.data()))
+    );
+  }
+
+  getMision(): Observable<any[]> {
+    return this.mision$;
+  }
+
+  getVision(): Observable<any[]> {
+    return this.vision$;
+  }
 }
