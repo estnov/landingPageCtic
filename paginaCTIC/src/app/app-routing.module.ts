bb2b9fe3f4100dab58b703d92558aed3de44fd53
Esteban <estebannovilloq@gmail.com>
2023-05-11 12:33:49 -0500

    Correction

--- paginaCTIC/src/app/app-routing.module.ts
+++ paginaCTIC/src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CticComponent } from './Components/ctic/ctic.component';
-import { ServiciosComponent } from './Components/servicios/servicios.component';
-import { BlogComponent } from './Components/blog/blog.component';
-import { ContactanosComponent } from './Components/contactanos/contactanos.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
-  { path: 'ctic', component: CticComponent },
-  { path: 'servicios', component: ServiciosComponent },
-  { path: 'blog', component: BlogComponent },
-  { path: 'contactanos', component: ContactanosComponent }
 ];
 
 @NgModule({
