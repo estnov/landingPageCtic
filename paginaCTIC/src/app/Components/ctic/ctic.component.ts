9bfadccf476a1a027f3bc73604975a512d4be9a2
Esteban <estebannovilloq@gmail.com>
2023-05-18 14:19:25 -0500

    Update

---
+++ paginaCTIC/src/app/Components/ctic/ctic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ctic',
+  templateUrl: './ctic.component.html',
+  styleUrls: ['./ctic.component.scss']
+})
+export class CticComponent {
+
+}
